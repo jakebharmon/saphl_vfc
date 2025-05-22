2e0640784cc6d1fa42d8a48da8cd53a9075cf99f
jakebharmon <jakebluesharmon@gmail.com>
2025-05-22 11:37:25 -0400

    SC

--- saphl_vfc.R
+++ saphl_vfc.R
@@ -1,3 +1,8 @@
 print("This file was created within RStudio")
 
 print("And now it lives on GitHub")
+
+usethis::create_github_token()
+#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
+gitcreds::gitcreds_set()
+gitcreds::gitcreds_get()
