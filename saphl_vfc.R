cc769d04671b1763dc6222b4de0ddeb933a2df51
jakebharmon <jakebluesharmon@gmail.com>
2025-05-22 14:46:44 -0400

    with map

--- saphl_vfc.R
+++ saphl_vfc.R
@@ -1,42 +1,33 @@
 ###Install packages
-install.packages("usmap")
-install.packages("tidygeocoder")
-install.packages("mapview")
-install.packages("writexl")
-library(usmap)
-library(tidyverse)
-library(readxl)
-library(tidygeocoder)
-library(mapview)
-library(writexl)
+#library(usmap)
+#library(tidyverse)
+#library(readxl)
+#library(tidygeocoder)
+#library(mapview)
+#library(writexl)
+#library(mapview)
 
-
-###Vaccines For Children Data
-vfc_providers <- read_excel("~/Desktop/ActiveVFCProviders.xlsx") %>% 
+### Vaccines For Children Data
+vfc_providers <- read_csv("https://github.com/jakebharmon/saphl_vfc/raw/refs/heads/main/data/ActiveVFCProviders.csv") %>% 
   mutate(County = str_to_title(County))
 
 vfc_providers[vfc_providers=="Mccormick"]<-"McCormick" #Standardize troublesome county name
 
-data(countypop) #call 2022 county South Carolina data from usmap
-
-vfc_coord<- vfc_providers %>%  #Currently produces 516/671 coordinates for location
-  mutate(address = str_c(`Address 1`, City, `State/Territory`, Zip,sep=", ")) %>% 
-  select(`Provider Name`, address) #%>%
-# geocode(address, method = 'osm', lat = latitude , long = longitude)
-
-###Medicaid data
+### Medicaid data
 #Rates of child vs adult vs senior
-medicaid_coverage_rates <- read.csv("~/Desktop/data-eUlDP.csv") %>% 
+medicaid_coverage_rates <- read.csv("https://github.com/jakebharmon/saphl_vfc/raw/refs/heads/main/data/data-eUlDP.csv") %>% 
   mutate(County.Name =  str_remove(County.Name," County"))
 
 #Amounts and Percentages of uninsured by adult and child
-insurance_gap <- read.csv("~/Desktop/Estimated Number & Percent without Health Insurance by County 2020.csv") %>% 
+insurance_gap <- read.csv("https://github.com/jakebharmon/saphl_vfc/raw/refs/heads/main/data/Estimated%20Number%20&%20Percent%20without%20Health%20Insurance%20by%20County%202020.csv") %>% 
   select(1,5,6,7)
 insurance_gap <- insurance_gap[-c(1,48,49),] #Remove extraneous cols
 insurance_gap$County <- str_replace_all(insurance_gap$County, " ", "") #Remove white space around text
 insurance_gap[,2]<-parse_number(insurance_gap[,2])
 insurance_gap[,3]<-parse_number(insurance_gap[,3])
-###Create joined dataset
+
+### Create joined dataset
+data(countypop) #call 2022 county South Carolina data from usmap
 sc_county_pop<-countypop %>% 
   filter(abbr=="SC") %>% 
   mutate(county = str_remove(county," County"))
@@ -60,29 +51,14 @@ vfc_per_county<-vfc_per_county%>%
     "child_unins_perc"=9
   ) %>% 
   mutate(chip_num=round(as.numeric(child_num)*chip_pcnt)/100) %>% 
-  mutate(patient_provider_ratio=(chip_num+child_unins_num)/vfc_num)
-summary(vfc_per_county)
+  mutate(patient_provider_ratio=round((chip_num+child_unins_num)/vfc_num))
 
 ###Data analysis
-plot(vfc_per_county$vfc_num,vfc_per_county$chip_num)
-plot(vfc_per_county$vfc_num,vfc_per_county$child_unins_num)
 
-plot_usmap(data=vfc_per_county,
-           linewidth = 0.1,
-           values="patient_provider_ratio",
-           region="counties",
-           include="SC") +
-  scale_fill_gradient(
-    labels = scales::label_number(big.mark = ','),
-    high = '#003366', #South Carolina Blue
-    low = 'white'
-  ) +
-  theme(legend.position = 'left', plot.title = element_text(face="bold")) +
-  labs(fill = 'Ratio',title="Ratio of Uninsured/CHIP Patients per VFC Provider") 
-)
 
-ggsave("sc_ppp_ratio.png")
-?ggsave
-mapview(vfc_per_county,zcol="patient_provider_ratio")
+# VA counties - downloaded via the awesome tigris package
+shape <- tigris::counties(state = "SC", class = "sf") %>% 
+  mutate(fips=paste0(STATEFP,COUNTYFP)) %>% 
+  full_join(vfc_per_county,shape,by="fips")
 
-write.csv(vfc_per_county, "/Users/jake/Desktop/purple air/vfc_per_county.csv")
+mapview(shape,zcol="patient_provider_ratio")
