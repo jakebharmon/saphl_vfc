80681914fccf38f19cda32e62cb8e912d271f711
jakebharmon <jakebluesharmon@gmail.com>
2025-05-24 17:20:16 -0400

    Adjusted colors on graph

--- saphl_vfc.R
+++ saphl_vfc.R
@@ -1,11 +1,7 @@
 ###Install packages
+#library(dplyr)
+#library(readr)
 #library(usmap)
-#library(tidyverse)
-#library(readxl)
-#library(tidygeocoder)
-#library(mapview)
-#library(writexl)
-#library(mapview)
 
 ### Vaccines For Children Data
 vfc_providers <- read_csv("https://github.com/jakebharmon/saphl_vfc/raw/refs/heads/main/data/ActiveVFCProviders.csv") %>% 
@@ -23,26 +19,29 @@ insurance_gap <- read.csv("https://github.com/jakebharmon/saphl_vfc/raw/refs/hea
   select(1,5,6,7)
 insurance_gap <- insurance_gap[-c(1,48,49),] #Remove extraneous cols
 insurance_gap$County <- str_replace_all(insurance_gap$County, " ", "") #Remove white space around text
-insurance_gap[,2]<-parse_number(insurance_gap[,2])
-insurance_gap[,3]<-parse_number(insurance_gap[,3])
+insurance_gap[,2]<-parse_number(insurance_gap[,2]) #Character -> Number
+insurance_gap[,3]<-parse_number(insurance_gap[,3]) #Character -> Number
 
 ### Create joined dataset
-data(countypop) #call 2022 county South Carolina data from usmap
+data(countypop) #Call 2022 county South Carolina data from usmap
 sc_county_pop<-countypop %>% 
   filter(abbr=="SC") %>% 
   mutate(county = str_remove(county," County"))
 
-vfc_per_county<-vfc_providers %>% #Makes table
+vfc_per_county<-vfc_providers %>% #Makes table of VFC's per county
   summarize(n=n(),.by=County) 
 
+#Join VFC with Census data
 vfc_per_county<-full_join(vfc_per_county,sc_county_pop,by=c("County"="county"))
 
+#Join VFCs with CHIP data
 vfc_per_county<-left_join(vfc_per_county,medicaid_coverage_rates[c(1,3)],by=c("County"="County.Name"))
 
+#Join VFCs with Uninsurance Data
 vfc_per_county<-left_join(vfc_per_county,insurance_gap,by=c("County"="County"))
 
 vfc_per_county<-vfc_per_county%>% 
-  rename(
+  rename( #Clean county names
     "county"=1,
     "vfc_num"=2,
     "chip_pcnt"=6,
@@ -51,14 +50,41 @@ vfc_per_county<-vfc_per_county%>%
     "child_unins_perc"=9
   ) %>% 
   mutate(chip_num=round(as.numeric(child_num)*chip_pcnt)/100) %>% 
-  mutate(patient_provider_ratio=round((chip_num+child_unins_num)/vfc_num))
+  mutate(patient_provider_ratio=round((chip_num+child_unins_num)/vfc_num)) %>% #Create ppr
+  arrange(county) %>% #Sort alphabetically for merging
+  mutate(grp=FALSE) #Create column for highlighting counties on graph
 
-### Data analysis
-
-
-# VA counties - downloaded via the awesome tigris package
-shape <- tigris::counties(state = "SC", class = "sf") %>% 
+### Data analysis 
+shape <- tigris::counties(state = "SC", class = "sf") %>% #Call county shape files
   mutate(fips=paste0(STATEFP,COUNTYFP)) %>% 
-  full_join(vfc_per_county,shape,by="fips")
+  full_join(vfc_per_county,shape,by="fips") %>% 
+  arrange(county)
 
-mapview(shape,zcol="patient_provider_ratio")
+#Create base graph
+p<-ggplot(vfc_per_county,aes(x=vfc_num,(child_unins_num+chip_num),color=grp),) +
+  geom_jitter(alpha=0.8,size=3,shape=18,width=3,height=3) +
+  labs(x="Number of VFC's Per County",
+       y="Number of Children with CHIP or No Insurance") +
+  theme_classic() +
+  theme(legend.position = "none")  +
+  scale_color_manual(breaks = c("TRUE","FALSE"),
+                   values=c("red","#E5E5E5"))
+
+#Replicate ggplot for each county
+p <- mget(rep("p", nrow(vfc_per_county)))
+
+#Tag each county in its respective graph
+for (i in 1:length(p)){
+  p[[i]]$data[i,12]=TRUE
+}
+
+p <- lapply(1:length(p), function(i) {
+  p[[i]]<-p[[i]] + labs(title=paste(p[[1]]$data[i,1],"County")) + theme( #Add county name as title
+    plot.title = element_text(face = "bold", size = 12,hjust = 0.5))  +#Bold
+  scale_alpha_ordinal()
+})
+
+#Create interactive map of South Carolina
+mapview::mapview(shape,zcol="patient_provider_ratio",
+        popup=leafpop::popupGraph(p, width = 300, height = 300))
+
