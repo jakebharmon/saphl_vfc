9dbcdc3ce9c5779e44f69b2e1c7a467bc82da471
jakebharmon <jakebluesharmon@gmail.com>
2025-06-14 13:19:28 -0400

    Redone with zip code data and purrr

--- saphl_vfc.R
+++ saphl_vfc.R
@@ -1,90 +1,118 @@
-###Install packages
-#library(dplyr)
-#library(readr)
-#library(usmap)
+### Load packages
+library(tidyverse)
+library(sf)
+library(mapview)
 
-### Vaccines For Children Data
-vfc_providers <- read_csv("https://github.com/jakebharmon/saphl_vfc/raw/refs/heads/main/data/ActiveVFCProviders.csv") %>% 
-  mutate(County = str_to_title(County))
+### Vaccines For Children Data South Carolina
 
-vfc_providers[vfc_providers=="Mccormick"]<-"McCormick" #Standardize troublesome county name
+# Read in VFC Data
+vfc_providers = read_csv(
+    "https://github.com/jakebharmon/saphl_vfc/raw/refs/heads/main/data/ActiveVFCProviders.csv"
+    ) %>%
+  mutate(
+    County = case_when(
+      County == "MCCORMICK" ~ "McCormick",  # Updates to proper CamelCase
+      TRUE ~ County %>%  str_to_title()     # Changes all UPPER CASE to Title
+    ),
+    address = paste(`Address 1`,            # Combines address data 
+                    `Address 2`,            # for geocoding
+                    City,
+                    `State/Territory`,
+                    Zip),
+    geometry = NA                           # Sets null column to hold geometry
+  )
 
-### Medicaid data
-#Rates of child vs adult vs senior
-medicaid_coverage_rates <- read.csv("https://github.com/jakebharmon/saphl_vfc/raw/refs/heads/main/data/data-eUlDP.csv") %>% 
-  mutate(County.Name =  str_remove(County.Name," County"))
+# Geocode data using Google Maps 
 
-#Amounts and Percentages of uninsured by adult and child
-insurance_gap <- read.csv("https://github.com/jakebharmon/saphl_vfc/raw/refs/heads/main/data/Estimated%20Number%20&%20Percent%20without%20Health%20Insurance%20by%20County%202020.csv") %>% 
-  select(1,5,6,7)
-insurance_gap <- insurance_gap[-c(1,48,49),] #Remove extraneous cols
-insurance_gap$County <- str_replace_all(insurance_gap$County, " ", "") #Remove white space around text
-insurance_gap[,2]<-parse_number(insurance_gap[,2]) #Character -> Number
-insurance_gap[,3]<-parse_number(insurance_gap[,3]) #Character -> Number
+#api = readLines("saphl_google_api.txt") # Read in API from .txt file
+#register_google(key = api)              # Register API key
+#getOption("ggmap")                      # Check that API Key is read
+#locations = vfc_providers$address %>%   # Geocodes addresses using Google API
+#  geocode()
+#
+#write_csv(locations, "vfc_geocode.csv")
 
-### Create joined dataset
-data(countypop) #Call 2022 county South Carolina data from usmap
-sc_county_pop<-countypop %>% 
-  filter(abbr=="SC") %>% 
-  mutate(county = str_remove(county," County"))
+vfc_geocode = read_csv("vfc_geocode.csv")
 
-vfc_per_county<-vfc_providers %>% #Makes table of VFC's per county
-  summarize(n=n(),.by=County) 
+vfc_providers[14] = st_as_sf(       # Fills geom column with sf of long/lat
+                                vfc_geocode, 
+                                coords = c("lon", "lat"), 
+                                crs = "NAD83", 
+                                agr = "constant"
+                        )
 
-#Join VFC with Census data
-vfc_per_county<-full_join(vfc_per_county,sc_county_pop,by=c("County"="county"))
+### Prepare census data on child poverty
 
-#Join VFCs with CHIP data
-vfc_per_county<-left_join(vfc_per_county,medicaid_coverage_rates[c(1,3)],by=c("County"="County.Name"))
+child_pov_acs =
+  tidycensus::get_acs(geography = "zip code tabulation area", #Chooses ZCTA
+          summary_var = 'B17020_001',  # Poverty by age table
+          variables = c("B17020_003",  # 003 = 0-6, 
+                        "B17020_004",  # 004 = 7-11
+                        "B17020_005"), # 005 = 12-17
+          geometry = TRUE,year=2020)   # 2020 last valid year for ACS data
 
-#Join VFCs with Uninsurance Data
-vfc_per_county<-left_join(vfc_per_county,insurance_gap,by=c("County"="County"))
+child_pov = child_pov_acs %>%
+  filter(substr(GEOID,1,2) == "29" &         # Filter for South Carolina code
+        summary_est > 0) %>%                 # Choose populations more than 0
+  select(GEOID, summary_est, estimate) %>%   # Select name, pop, child w/ pov est
+  group_by(GEOID) %>%                        # Group by name
+  summarize(child_pov = sum(estimate)) %>%   # Sum into single child pov metric
+  mutate(centroid = 
+           st_centroid(st_geometry(geometry))# Create centroids of shapefiles
+         )
 
-vfc_per_county<-vfc_per_county%>% 
-  rename( #Clean county names
-    "county"=1,
-    "vfc_num"=2,
-    "chip_pcnt"=6,
-    "child_num"=7,
-    "child_unins_num"=8,
-    "child_unins_perc"=9
-  ) %>% 
-  mutate(chip_num=round(as.numeric(child_num)*chip_pcnt)/100) %>% 
-  mutate(patient_provider_ratio=round((chip_num+child_unins_num)/vfc_num)) %>% #Create ppr
-  arrange(county) %>% #Sort alphabetically for merging
-  mutate(grp=FALSE) #Create column for highlighting counties on graph
+haversine = (
+  st_distance(
+    child_pov$centroid,                 # Calculate distance from each VFC
+    vfc_providers$geometry)             # Provider to county centroid
+    * 0.000621371) %>%                  # Meters -> Miles
+  data.frame() %>% 
+  map( ~ as.numeric(.x)) %>%            # Convert class from units to numeric
+  map_df( ~ case_when(.x > 10 ~ FALSE,  # Convert to False if distance > 10 miles
+                 .x < 10 ~ TRUE)) %>% 
+  mutate(
+    GEOID = child_pov[[1]],        # Select first column from geom tibble
+    rowsum = pmap_dbl(., sum)) %>% # Sum Trues by rows to find number of VFC's
+  select(GEOID, rowsum) %>%        # In 10 mile radius of centroids
+  inner_join(child_pov, by="GEOID") %>% # Join with child_pov data 
+  mutate(child_per_vfc = 
+           ifelse(
+             is.finite((child_pov / rowsum)), # Preventing inf and NaN
+             (child_pov / rowsum),            # Number of impov chlrn by vfc
+             0)   
+         ) %>% 
+  mutate(
+    vfc_breaks = cut(rowsum,      # Bin VFC rowsums for graphing
+                 breaks=c(-Inf,2,10,30,Inf)),
+    ratio_breaks = cut(child_per_vfc,      # Bin child/vfc ratio for graphing
+                      breaks=c(-Inf,100,500,1000,Inf)))
 
-### Data analysis 
-shape <- tigris::counties(state = "SC", class = "sf") %>% #Call county shape files
-  mutate(fips=paste0(STATEFP,COUNTYFP)) %>% 
-  full_join(vfc_per_county,shape,by="fips") %>% 
-  arrange(county)
-
-#Create base graph
-p<-ggplot(vfc_per_county,aes(x=vfc_num,(child_unins_num+chip_num),color=grp),) +
-  geom_jitter(alpha=0.8,size=3,shape=18,width=3,height=3) +
-  labs(x="Number of VFC's Per County",
-       y="Number of Children with CHIP or No Insurance") +
-  theme_classic() +
-  theme(legend.position = "none")  +
-  scale_color_manual(breaks = c("TRUE","FALSE"),
-                   values=c("red","#E5E5E5"))
-
-#Replicate ggplot for each county
-p <- mget(rep("p", nrow(vfc_per_county)))
-
-#Tag each county in its respective graph
-for (i in 1:length(p)){
-  p[[i]]$data[i,12]=TRUE
+#Create function of graph
+plot_ratio = function(entity) {
+  ggplot(haversine, aes(x = rowsum, y = child_pov)) +
+    geom_point(data = filter(haversine, GEOID %in% entity), 
+               color = "red", alpha = 1, size = 4) +
+    geom_point(data = filter(haversine, !GEOID %in% entity), 
+               shape = 1, color = "grey45", size = .2) +
+    labs(x = "Number of VFC in 10 Mile Radius", 
+         y = "Number of Impoverished Children") +
+    theme_bw()
 }
 
-p <- lapply(1:length(p), function(i) {
-  p[[i]]<-p[[i]] + labs(title=paste(p[[1]]$data[i,1],"County")) + theme( #Add county name as title
-    plot.title = element_text(face = "bold", size = 12,hjust = 0.5))  +#Bold
-  scale_alpha_ordinal()
-})
+p = haversine$GEOID %>% #It is important to have $ rather than [],
+                        #As $ returns a list and [] a data frame object
+  map( ~ plot_ratio(entity = .x)) # Map plot function across GEOID's
+# p[[1]] Verify
 
 #Create interactive map of South Carolina
-mapview::mapview(shape,zcol="patient_provider_ratio",
+sc_plot = tigris::zctas(state="SC",year=2010) %>% #Call ZCTA shape files
+  inner_join(haversine[c(1,6)],      # Select GEOID and child_per_vfc
+             by = join_by(
+               "ZCTA5CE10"=="GEOID") # Match tigris colname
+             ) %>% 
+  arrange(ZCTA5CE10)                 # Matches purred maps to correct shapefile
+
+sc_map<-mapview::mapview(sc_plot,zcol="child_per_vfc",
         popup=leafpop::popupGraph(p, width = 300, height = 300))
 
+mapview::mapshot(sc_map,url = paste0(getwd(), "/sc_map.html"))
